de88cfa0f777e33d9a9f9b84cd605230d92f89f9
LukasPokryvka <58267318+LukasPokryvka@users.noreply.github.com>
2025-10-27 19:54:47 +0100

    Update proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -1,37 +1,18 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 import createMiddleware from 'next-intl/middleware'
 import { routing } from './i18n/routing'
-import { ROUTES } from './lib/routes'
 
 const intlMiddleware = createMiddleware(routing)
 
-const PUBLIC_ROUTES = [ROUTES.Landing(), /^\/api\/webhooks(.*)/]
+const isPublicRoute = createRouteMatcher(['/', '/(en|sk)', '/api/webhooks(.*)'])
 
 export const proxy = clerkMiddleware(async (auth, req) => {
-  const { pathname } = req.nextUrl
-
   const response = intlMiddleware(req)
-
-  const isPublic = PUBLIC_ROUTES.some(route =>
-    route instanceof RegExp ? route.test(pathname) : route === pathname
-  )
-
-  if (isPublic) {
-    return response
-  }
-
+  if (isPublicRoute(req)) return response
   await auth.protect()
-
   return response
 })
 
-// 4️⃣ Matcher for both Clerk & next-intl
 export const config = {
-  matcher: [
-    // Match all pathnames except for
-    // - api, trpc, _next, _vercel, and files with extensions
-    '/((?!api|trpc|_next|_vercel|.*\\..*).*)',
-    // Also match api/trpc routes (for Clerk)
-    '/(api|trpc)(.*)'
-  ]
+  matcher: ['/((?!api|trpc|_next|_vercel|.*\\..*).*)', '/(api|trpc)(.*)']
 }
